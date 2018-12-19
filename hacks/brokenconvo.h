e28fff8c635f943b561462ed635e9859bb4387bf
Noah Weninger <nweninge@ualberta.ca>
2018-12-19 13:36:03 -0700

    the classics in stereo

--- hacks/brokenconvo.h
+++ hacks/brokenconvo.h
@@ -1,8 +1,10 @@
 const int fft_size=1<<16;
 void init() {}
 void fill(cplx *buf[2], int n, double t) {
-    for (int i = 1; i < n; i++) {
-        int r = (int)(t*M_PI)*n/2+i;
-        buf[0][i]=buf[1][i]=sin(2.0*M_PI*(i/(1+fmod((1+(double)(r/77&r/38&r/24)),i+1))))/pow(i+1, 0.75)/2;
+    for (int c = 0; c < 2; c++) {
+        for (int i = 1; i < n; i++) {
+            int r = (int)(t*M_PI)*n/2+i;
+            buf[c][i]=sin(2.0*M_PI*(i/(1+fmod((1+(double)(r/77&r/38&r/24+c)),i+1))))/pow(i+1, 0.75)/2;
+        }
     }
 }
--- hacks/voidbubbles.h
+++ hacks/voidbubbles.h
@@ -1,8 +1,10 @@
 const int fft_size=1<<13;
 void init() {}
 void fill(cplx *buf[2], int n, double t) {
-    for (int i = 1; i < n; i++) {
-        int r = (int)(t*32)*n+i;
-        buf[0][i]=buf[1][i]=sin(2.0*M_PI/64.*(((int)pow(i,1.1))&r/3|r/250|r/136|r>>16))/pow(i+1, 0.7);
+    for (int c = 0; c < 2; c++) {
+        for (int i = 1; i < n; i++) {
+            int r = (int)(t*32)*n+i;
+            buf[c][i]=sin(2.0*M_PI/64.*(c+((int)pow(i,1.1))&r/3|r/250|r/136|r>>16))/pow(i+1, 0.7);
+        }
     }
 }
